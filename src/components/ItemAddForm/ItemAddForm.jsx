d8600e2ff81eb5510d2985b2b48fda26b74bfa20
Slaventy1969 <slaventy.69@gmail.com>
2022-08-27 19:11:01 +0300

    Implementation input change text

--- src/components/ItemAddForm/ItemAddForm.jsx
+++ src/components/ItemAddForm/ItemAddForm.jsx
@@ -1,24 +1,41 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import './ItemAddForm.css';
 
 export default class ItemAddForm extends Component {
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdd(this.state.label);
+        //       this.setState({ label: '' });
+    }
     render() {
         return (
-            <div className="input-group mb-3 item-add-form">
+            <form className="input-group mb-3 item-add-form"
+                onSubmit={this.onSubmit}
+            >
                 <input
                     type="text"
                     className="form-control"
                     placeholder='Новий Todo ...'
-                    onChange={(event) => console.log(event.target.value)}
+                    onChange={this.onLabelChange}
                 />
                 <div className="input-group-append">
                     <button
                         className="btn btn-outline-secondary"
-                        onClick={() => this.props.onItemAdd('tteexxtt')}>
+                    >
                         Додати
                     </button>
                 </div>
-            </div>
+            </form>
         )
     }
 }
